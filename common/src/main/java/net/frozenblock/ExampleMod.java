0f9c303fe5d3358529e9a8198208109719a7e544
AlexTheDolphim0 <79395212+AlexTheDolphin0@users.noreply.github.com>
2024-07-12 06:54:41 -0500

    Initial commit

---
+++ common/src/main/java/net/frozenblock/ExampleMod.java
@@ -0,0 +1,9 @@
+package net.frozenblock;
+
+public final class ExampleMod {
+    public static final String MOD_ID = "very_blue_stair_mod";
+
+    public static void init() {
+        // Write common init code here.
+    }
+}
