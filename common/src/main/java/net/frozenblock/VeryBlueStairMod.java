28c925e682f055a9bb813511a593d67045cec278
AlexTheDolphim0 <79395212+AlexTheDolphin0@users.noreply.github.com>
2024-07-12 08:14:46 -0500

    Delete artifact from prior testing

--- common/src/main/java/net/frozenblock/VeryBlueStairMod.java
+++
@@ -1,9 +0,0 @@
-package net.frozenblock;
-
-public final class ExampleMod {
-    public static final String MOD_ID = "very_blue_stair_mod";
-
-    public static void init() {
-        // Write common init code here.
-    }
-}
